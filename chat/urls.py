7fb8f07ab90bf6d2654ccaef4e3c7d8ae46af610
Son Haechang <sonhaechang@Sonui-MacBookPro.local>
2024-04-07 03:21:31 +0900

    feat: Implementation of show chat room list function
    1. views.py
       - 채팅방 목록 확인 요청을 처리하는 view
    2. urls.py
       - urlpatterns에 '' 경로와 RolePlayingRoomListView 매핑
    3. tempalates
       - create container/role_playing_room_list.html

--- chat/urls.py
+++ chat/urls.py
@@ -6,6 +6,7 @@ from chat import views
 app_name = 'chat'
 
 urlpatterns = [
+    path('', views.RolePlayingRoomListView.as_view(), name='role_playing_room_list'),
     path('new/', views.RolePlayingRoomCreateView.as_view(), name='role_playing_room_new'),
     path('<int:pk>/edit/', views.RolePlayingRoomUpdateView.as_view(), name='role_playing_room_edit'),
 ]
--- chat/views.py
+++ chat/views.py
@@ -5,13 +5,25 @@ from django.forms import BaseModelForm
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.utils.decorators import method_decorator
-from django.views.generic import CreateView, UpdateView
+from django.views.generic import ListView, CreateView, UpdateView
 
 from chat.forms import RolePlayingRoomForm
 from chat.models import RolePlayingRoom
 
 
 # Create your views here.
+@method_decorator(staff_member_required, name='dispatch')
+class RolePlayingRoomListView(ListView):
+    model = RolePlayingRoom
+    template_name = 'chat/container/role_playing_room_list.html'
+
+    def get_queryset(self) -> QuerySet[Any]:
+        qs = super().get_queryset()
+        qs = qs.filter(user=self.request.user)
+
+        return qs
+
+
 @method_decorator(staff_member_required, name='dispatch')
 class RolePlayingRoomCreateView(CreateView):
     model = RolePlayingRoom
@@ -21,6 +33,7 @@ class RolePlayingRoomCreateView(CreateView):
     def form_valid(self, form: BaseModelForm) -> HttpResponse:
         role_playing_room = form.save(commit=False)
         role_playing_room.user = self.request.user
+
         return super().form_valid(form)
     
 
