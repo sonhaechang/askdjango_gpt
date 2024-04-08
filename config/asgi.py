97d07552cc58e7068d08ea524e65c3ed72db4c9f
Son Haechang <sonhaechang@Sonui-MacBookPro.local>
2024-04-08 18:43:03 +0900

    refector: Socket user's chat room confirm and initial prompt inquiry
    1. config/asgi.py
       - AuthMiddlewareStack사용해서 websocket ASGI에서 쿠키, 세션, 인증을 추가로 사용가능하게 수정
    2. chat/consumers.py
       - create get_room method (소켓 접속하는 유저가 로그인 중이고 채팅방이 있다면 반환 없다면 None 반환)
       - modify __init__ method (대화 내역을 담을 gpt_messages 변수 선언)
       - create connect method (get_room이 None이면 요청 거절, None이 아니라면 요청 수락)
       - modify receive_json (불필요한 코드 제거)

--- config/asgi.py
+++ config/asgi.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 
 import os
 
+from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 
@@ -20,7 +21,9 @@ django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
     'http': django_asgi_app,
-    'websocket': URLRouter(
-        chat.routing.websocket_urlpstterns
+    'websocket': AuthMiddlewareStack(
+        URLRouter(
+            chat.routing.websocket_urlpstterns
+        ),
     ),
 })
